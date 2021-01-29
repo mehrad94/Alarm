73aa5d8b89bca238ba6381fa3100b94ed4479560
Mehrad Farahnak <mehradfarahnak9@gmail.com>
2021-01-30 00:15:58 +0330

    s

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 var http = require("http");
 const WebSocket = require("ws");
+const axios = require("axios");
+
 http
   .createServer(function (req, res) {
     res.write("Hello World!"); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
+
+// const token = "1454264149:AAEA5kuWlIVtiXBSg8N7pRPvuG3AGaTwg6E";
 
 const setAlarm = (crypto, price) => {
   const ws = new WebSocket(`wss://fstream.binance.com/ws/${crypto}@markPrice`);
@@ -18,4 +22,18 @@ const setAlarm = (crypto, price) => {
   });
 };
 
-setAlarm("btcusdt", "36900");
+const telegramBot = async () => {
+  // https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe
+
+  try {
+    const resAxios = await axios.get(
+      "https://api.telegram.org/1454264149:AAEA5kuWlIVtiXBSg8N7pRPvuG3AGaTwg6E/sendMessage"
+    );
+    console.log({ resAxios });
+  } catch (e) {
+    console.log({ e });
+  }
+};
+
+// setAlarm("btcusdt", "36900");
+telegramBot();
