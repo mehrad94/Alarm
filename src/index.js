19513068403936a023bf0dad6e43aecd5ad760c1
Mehrad Farahnak <mehradfarahnak9@gmail.com>
2021-01-29 20:22:43 +0330

    Connect to api and get price

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 var http = require("http");
-
-//create a server object:
+const WebSocket = require("ws");
 http
-  .createServer(function(req, res) {
+  .createServer(function (req, res) {
     res.write("Hello World!"); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
+
+const setAlarm = (crypto, price) => {
+  const ws = new WebSocket(`wss://fstream.binance.com/ws/${crypto}@markPrice`);
+  ws.on("message", (data) => {
+    const details = JSON.parse(data);
+    console.log({ MarkPrice: parseFloat(details.p) });
+    if (parseFloat(details.p) === price) {
+      console.log("================== reached =================");
+    }
+  });
+};
+
+setAlarm("btcusdt", "36900");
