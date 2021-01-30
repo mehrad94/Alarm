0f706ea14863b60866d9d9331f69e6b5d0072d29
Mehrad Farahnak <mehradfarahnak9@gmail.com>
2021-01-30 18:14:40 +0330

    de

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var http = require("http");
 const WebSocket = require("ws");
-const axios = require("axios");
-
+const TelegramBot = require("node-telegram-bot-api");
 http
   .createServer(function (req, res) {
     res.write("Hello World!"); //write a response to the client
@@ -11,29 +10,80 @@ http
 
 // const token = "1454264149:AAEA5kuWlIVtiXBSg8N7pRPvuG3AGaTwg6E";
 
-const setAlarm = (crypto, price) => {
-  const ws = new WebSocket(`wss://fstream.binance.com/ws/${crypto}@markPrice`);
-  ws.on("message", (data) => {
-    const details = JSON.parse(data);
-    console.log({ MarkPrice: parseFloat(details.p) });
-    if (parseFloat(details.p) === price) {
-      console.log("================== reached =================");
-    }
-  });
-};
+// const bot = new TelegramBot(token, { polling: true });
 
-const telegramBot = async () => {
-  // https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe
+// const setPriceDown = (crypto, price, chatId) => {
+//   const ws = new WebSocket(`wss://fstream.binance.com/ws/${crypto}@markPrice`);
+//   ws.on("message", (data) => {
+//     const details = JSON.parse(data);
+//     console.log({ MarkPrice: parseFloat(details.p) });
+//     if (parseFloat(details.p) <= price) {
+//       console.log("================== reached =================");
+//       bot.sendMessage(chatId, `${crypto} Reached to ${price}`);
+//       ws.close();
+//     }
+//   });
+// };
+// const setPriceUp = (crypto, price, chatId) => {
+//   const ws = new WebSocket(`wss://fstream.binance.com/ws/${crypto}@markPrice`);
+//   ws.on("message", (data) => {
+//     const details = JSON.parse(data);
+//     console.log({ MarkPrice: parseFloat(details.p) });
+//     if (parseFloat(details.p) >= price) {
+//       console.log("================== reached =================");
+//       bot.sendMessage(chatId, `${crypto} Reached to ${price}`);
+//       ws.close();
+//     }
+//   });
+// };
+// const telegramBot = async () => {
+//   // bot.on("message", (s) => console.log({ s }));
+//   bot.onText(/\/priceup (.+)/, (msg, match) => {
+//     const chatId = msg.chat.id;
+//     const resp = match[1]; // the captured "whatever"
+//     console.log({ resp });
+//     bot.sendMessage(chatId, resp);
+//   });
+//   bot.onText(/\/priceDown (.+)/, (msg, match) => {
+//     const chatId = msg.chat.id;
+//     const details = match[1].split(" ");
 
-  try {
-    const resAxios = await axios.get(
-      "https://api.telegram.org/1454264149:AAEA5kuWlIVtiXBSg8N7pRPvuG3AGaTwg6E/sendMessage"
-    );
-    console.log({ resAxios });
-  } catch (e) {
-    console.log({ e });
-  }
-};
+//     console.log({ PAIR: details[0], PRICE: details[1] });
+//     bot.sendMessage(
+//       chatId,
+//       `Alarm set for ${details[0]} When Price Reached to ${details[1]}`
+//     );
+//   });
+//   bot.onText(/\/help (.+)/, (msg, match) => {
+//     const chatId = msg.chat.id;
+//     const helpMessage = `
+//     /priceDown for setting alarm when price bigger down current price /n
+//     `;
+//     const resp = match[1]; // the captured "whatever"
+//     bot.sendMessage(chatId, resp);
+//   });
+//   bot.on("message", (m) => {
+//     if (m.text.includes("priceUp")) {
+//       const details = m.text.split(" ");
+//       if (details[1] != null && details[2] != null) {
+//         setPriceUp(details[1], details[2], m.chat.id);
+//         bot.sendMessage(
+//           m.chat.id,
+//           `Alarm set for ${details[1]} When Price Reached to ${details[2]}`
+//         );
+//       } else bot.sendMessage(m.chat.id, "Command not recognize");
+//     } else if (m.text.includes("priceDown")) {
+//       const details = m.text.split(" ");
+//       if (details[1] != null && details[2] != null) {
+//         setPriceDown(details[1], details[2], m.chat.id);
+//         bot.sendMessage(
+//           m.chat.id,
+//           `Alarm set for ${details[1]} When Price Reached to ${details[2]}`
+//         );
+//       } else bot.sendMessage(m.chat.id, "Command not recognize");
+//     }
+//   });
+// };
 
-// setAlarm("btcusdt", "36900");
-telegramBot();
+// // telegramBot();
+// // setAlarm("btcusdt", "");
